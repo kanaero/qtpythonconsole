16a4c4146d99c5b25a7940fd205c009057d0219e
Suresh K. Kannan <kannan@nodein.com>
2017-08-28 22:07:59 -0600

    console works but stdout still prints on console

--- qpythonconsole.cpp
+++ qpythonconsole.cpp
@@ -1,14 +1,45 @@
 #include "qpythonconsole.h"
 #include <QColor>
 #include <pybind11/embed.h>
+#include <iostream>
+#include <strstream>
+#include <sstream>
+
+
 namespace py = pybind11;
+
+struct cout_redirect {
+	cout_redirect(std::streambuf * new_buffer)
+		: old(std::cout.rdbuf(new_buffer))
+	{ }
+
+	~cout_redirect() {
+		std::cout.rdbuf(old);
+	}
+
+private:
+	std::streambuf * old;
+};
+
+struct cerr_redirect {
+	cerr_redirect(std::streambuf * new_buffer)
+		: old(std::cerr.rdbuf(new_buffer))
+	{ }
+
+	~cerr_redirect() {
+		std::cerr.rdbuf(old);
+	}
+
+private:
+	std::streambuf * old;
+};
 
 QPythonConsole::QPythonConsole(QWidget *parent, const QString &welcomeText) : QConsole(parent, welcomeText) {
 	connect(
 		this, &QConsole::execCommand,
 		this, &QPythonConsole::ExecuteAndPrintResults
 	);
-	this->setPrompt(">>");
+	this->setPrompt(">>>");
 	QFont f("unexistent");
 	f.setStyleHint(QFont::Monospace);
 	this->setFont(f);
@@ -24,13 +55,16 @@ QPythonConsole::~QPythonConsole() {
 
 void QPythonConsole::ExecuteAndPrintResults(const QString &command) {
 	QByteArray ba = command.toLocal8Bit();
-	try {
-		auto result = py::eval(ba.data());
-		this->printCommandExecutionResults(command, QConsole::Complete);
+	std::stringbuf  coutstream;
+	std::stringbuf  cerrstream;
 
+	cout_redirect cout_guard(&coutstream);
+	cerr_redirect cerr_guard(&cerrstream);
+	try {
+		auto result = py::eval<py::eval_single_statement>(ba.data());
+		this->printCommandExecutionResults(coutstream.str().c_str(), QConsole::Complete);
 	}
 	catch (py::error_already_set &e) {
-		this->printCommandExecutionResults(command, QConsole::Complete);
-
+		this->printCommandExecutionResults(e.what(), QConsole::Error);
 	}
 }
