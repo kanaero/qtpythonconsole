3b0e27162d66ed050a5a3bffac09daa5c6ff43fe
Suresh Kannan <kannan@nodein.com>
2017-09-05 17:24:04 -0400

    adding a redirector

--- qpythonconsole.cpp
+++ qpythonconsole.cpp
@@ -7,67 +7,30 @@
 #include <pybind11/iostream.h>
 
 namespace py = pybind11;
-const std::string blah = R"(
-from contextlib import contextmanager
-import ctypes
-import io
-import os, sys
-import tempfile
-
-libc = ctypes.CDLL(None)
-c_stdout = ctypes.c_void_p.in_dll(libc, 'stdout')
-
-@contextmanager
-def stdout_redirector(stream) :
-    # The original fd stdout points to.Usually 1 on POSIX systems.
-    original_stdout_fd = sys.stdout.fileno()
-
-    def _redirect_stdout(to_fd) :
-        """Redirect stdout to the given file descriptor."""
-        # Flush the C - level buffer stdout
-        libc.fflush(c_stdout)
-        # Flush and close sys.stdout - also closes the file descriptor(fd)
-        sys.stdout.close()
-        # Make original_stdout_fd point to the same file as to_fd
-        os.dup2(to_fd, original_stdout_fd)
-        # Create a new sys.stdout that points to the redirected fd
-        sys.stdout = io.TextIOWrapper(os.fdopen(original_stdout_fd, 'wb'))
-
-        # Save a copy of the original stdout fd in saved_stdout_fd
-        saved_stdout_fd = os.dup(original_stdout_fd)
-        try :
-            # Create a temporary file and redirect stdout to it
-            tfile = tempfile.TemporaryFile(mode = 'w+b')
-            _redirect_stdout(tfile.fileno())
-            # Yield to caller, then redirect stdout back to the saved fd
-            yield
-            _redirect_stdout(saved_stdout_fd)
-            # Copy contents of temporary file to the given stream
-            tfile.flush()
-            tfile.seek(0, io.SEEK_SET)
-            stream.write(tfile.read())
-        finally:
-            tfile.close()
-            os.close(saved_stdout_fd)
-)";
 
 
 
-// See below for information on redirection of str3eams.
-//http://eli.thegreenplace.net/2015/redirecting-all-kinds-of-stdout-in-python/
-
-struct stdout_redirect {
-	stdout_redirect(std::streambuf * new_buffer)
-		: old(std::cout.rdbuf(new_buffer))
-	{ }
-
-	~stdout_redirect() {
-		std::cout.rdbuf(old);
+class Redirector {
+public:
+	Redirector() {}
+	virtual ~Redirector() {}
+	void write(std::string text) {
+		out_text.append(text.c_str());
 	}
-
-private:
-	std::streambuf * old;
+	QString out_text;
 };
+
+Redirector redirect_stream;
+
+PYBIND11_EMBEDDED_MODULE(redirector, m) {
+	m.doc() = "pybind11 stdout/stderr redirection module";
+	py::class_<Redirector>(m, "Redirector")
+		.def("write", &Redirector::write)
+	py::object redirect_stream_obj = py::cast("Redirector");
+	m.attr("redirector_stream_obj") = redirect_stream;
+}
+
+
 
 struct cout_redirect {
 	cout_redirect(std::streambuf * new_buffer)
@@ -95,6 +58,7 @@ private:
 	std::streambuf * old;
 };
 
+
 QPythonConsole::QPythonConsole(QWidget *parent, const QString &welcomeText) : QConsole(parent, welcomeText) {
 	connect(
 		this, &QConsole::execCommand,
@@ -104,9 +68,16 @@ QPythonConsole::QPythonConsole(QWidget *parent, const QString &welcomeText) : QC
 	QFont f("unexistent");
 	f.setStyleHint(QFont::Monospace);
 	this->setFont(f);
-
-
-
+	
+	//py::module::import("sys").attr("stdout") = py::module::import("redirector").attr("write");
+	try {
+		py::exec("import redirector");
+		py::exec("import sys");
+		py::exec("sys.stdout = redirector.write");
+		py::exec("print('asdfasdfasf')");
+	} catch (py::error_already_set &e) {
+		this->printCommandExecutionResults(e.what(), QConsole::Error);
+	}
 }
 
 QPythonConsole::~QPythonConsole() {
